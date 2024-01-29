a4a30e4faac2e75aaa42a2296bd5fdd014c9ce40
TheTisiboth <thetisiboth@gmail.com>
2024-01-29 11:47:30 +0100

    #9: Add db setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
-export default function Home() {
+import prisma from "./lib/prisma"
+
+export default async function Home() {
+  const recipes = await prisma.recipe.findMany()
+
   return (
     <main>
       <div>
         <h1>Home</h1>
+        <p>Recipe: {recipes[0]?.title}</p>
       </div>
     </main>
   )
