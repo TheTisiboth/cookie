897ad5de3bfc409639df2779c391108c64a496ec
TheTisiboth <thetisiboth@gmail.com>
2024-01-29 13:22:54 +0100

    #5: Add sonner toast #10 Add zod

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,20 @@
-import prisma from "./lib/prisma"
+import prisma from "@/lib/prisma"
+import { RecipeComponent } from "./Recipe"
 
 export default async function Home() {
   const recipes = await prisma.recipe.findMany()
+  const user = await prisma.user.findFirst()
+  console.log(recipes)
+  console.log("Recipe loaded")
 
   return (
     <main>
-      <div>
-        <h1>Home</h1>
-        <p>Recipe: {recipes[0]?.title}</p>
-      </div>
+      <h1>Home</h1>
+      <h2>User</h2>
+      <p>User: {user?.name}</p>
+      {recipes.map((recipe) => (
+        <RecipeComponent key={recipe.id} {...recipe} />
+      ))}
     </main>
   )
 }
